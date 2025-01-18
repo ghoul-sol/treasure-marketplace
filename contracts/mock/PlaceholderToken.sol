d8bb64812e9311d0e6cf9338ef194e14f8b3c33f
Mark Vujevits <vujevits.mark@gmail.com>
2025-01-18 20:50:08 +0100

    fix build

--- contracts/mock/PlaceholderToken.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract PlaceholderToken is ERC20("Placeholder Token", "PLACEHOLDER") {}
